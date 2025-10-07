158c7d6b398a77518f42630280c6c65639ed891e
Marek Roque <marekroque58@gmail.com>
2025-10-07 17:15:27 +0200

    Fixed navigation bar and began format of featured projects

--- scripts/stickyNavBar.js
+++ scripts/stickyNavBar.js
@@ -1,16 +1,28 @@
 const nav = document.getElementById('navbar');
 let hasScrolled = false;
+
 nav.style.marginTop = '40vh';
 
+nav.classList.remove('is-stuck');
+
 function checkSticky() {
-    const navTop = nav.getBoundingClientRect().top;
-    
-    if (navTop <= 0) {
-        nav.classList.add('is-stuck');
-    } else {
-        nav.classList.remove('is-stuck');
-    }
+  if (window.scrollY === 0) {
+    nav.classList.remove('is-stuck');
+    return;
+  }
+  
+  const navTop = nav.getBoundingClientRect().top;
+  if (navTop <= 0) {
+    nav.classList.add('is-stuck');
+  } else {
+    nav.classList.remove('is-stuck');
+  }
 }
 
 window.addEventListener('scroll', checkSticky);
-checkSticky();
+
+setTimeout(() => {
+  if (window.scrollY > 0) {
+    checkSticky();
+  }
+}, 0);
